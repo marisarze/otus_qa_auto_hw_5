6c03e38252c184e433708ecef97f5395902e498f
marisarze <flypetrov@yandex.ru>
2022-08-28 08:16:05 +0300

    raw 3

--- tests/test_open_cart.py
+++ tests/test_open_cart.py
@@ -1,10 +1,13 @@
 import pytest
 from conftest import driver, url
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from exception_wrappers import wait, wait_title
 
 
 def test_main(driver, url):
     driver.get(url=url)
     driver.save_screenshot("test.png")
     assert driver.title == "Your Store"
+    wait
